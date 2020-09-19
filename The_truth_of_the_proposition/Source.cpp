a7cedc60bf573a40bf95afa08851180d8634b195
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-09-19 16:19:41 +0700

    80 %

--- The_truth_of_the_proposition/Source.cpp
+++ The_truth_of_the_proposition/Source.cpp
@@ -1,7 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include <string.h>
-
+#include <conio.h> 
+#include <dos.h>  
+#include <stdio.h> 
+#include <string.h> 
 
 
 int main()
@@ -9,12 +10,13 @@ int main()
 	struct charstr
 	{
 		char Letters[4];
+		int i;
 	}str;
 
 	printf("InPut : ");
-	for (int i = 0; i < 4; i++)
+	for (str.i = 0; str.i < 4; str.i++)
 	{
-		scanf_s("%s",&str.Letters[i]);
+		scanf_s("%s",&str.Letters[str.i]);
 		getchar();
 		
 	}
@@ -25,4 +27,57 @@ printf("%s", str.Letters[0]);
 		printf("T");
 	}*/
 	return 0;
+}
+
+/// <summary>
+/// ////////////////////
+/// </summary>
+/// <param name=""></param>
+void inputpassword(void);
+
+void outputpassword(void);
+
+struct pass
+{
+	char password[9];
+	int check;
+}gotoo;
+
+
+
+int main()
+{
+
+	inputpassword();
+	printf("Please push Enter .\n");
+	_getch();
+	outputpassword();
+
+	_getch();
+}
+
+void inputpassword()
+{
+	printf("Enter Password: ");
+
+	for (gotoo.check = 0; gotoo.check < 8; gotoo.check++) {
+
+
+		gotoo.password[gotoo.check] = _getch();
+
+
+		printf("*");
+	}
+	gotoo.password[gotoo.check] = '\0';
+	printf("\n");
+}
+
+void outputpassword()
+{
+	printf("Show password: ");
+	if (gotoo.password[gotoo.check] != '\0' || gotoo.password[gotoo.check] != 'x') {
+		for (gotoo.check = 0; gotoo.password[gotoo.check] != '\0'; gotoo.check++)
+			printf("%c", gotoo.password[gotoo.check]);
+	}
+
 }
